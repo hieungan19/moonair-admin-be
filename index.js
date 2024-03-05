3861a39ba05cf33f3d138845529ae50a07b061c5
Hieu Ngan <21520358@gm.uit.edu.vn>
2024-03-05 07:44:49 +0700

    dev

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Init code');
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+  res.end('Hello world!');
+});
+server.listen(3000, function(){
+    console.log("Server listening on http://localhost:3000")
+});
